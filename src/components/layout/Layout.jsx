9b4b974cb50724ad81df923926c53c9678b7e2a5
Awes0meX <102291000+Aweso0meX@users.noreply.github.com>
2022-05-17 18:50:34 +0300

    123

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { Outlet } from 'react-router-dom'
 import { activeMenuAC } from '../../redux/headerReducer/actions'
 import { HeaderBuisness } from './Header/HeaderBuisness'
